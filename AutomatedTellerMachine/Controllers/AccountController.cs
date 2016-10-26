4deb19c2d75b2c695e50a48538b3ad2dbbcf64b2
t-ho <t.ho26010@gmail.com>
2016-10-26 16:44:00 +1000

    Add additional custom properties in the authentication cookie

--- AutomatedTellerMachine/Controllers/AccountController.cs
+++ AutomatedTellerMachine/Controllers/AccountController.cs
@@ -156,6 +156,7 @@ namespace AutomatedTellerMachine.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+                    UserManager.AddClaim(user.Id, new Claim(ClaimTypes.GivenName, model.FirstName));
                     var service = new CheckingAccountService(HttpContext.GetOwinContext().Get<ApplicationDbContext>());
                     service.CreateCheckingAccount(model.FirstName, model.LastName, user.Id, 0);
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
